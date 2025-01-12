e4998be2619fa2ee4c2e70fa20c6389054831809
Zarbba <zarbba@hotmail.com>
2025-01-13 10:58:01 +1100

    Adds basic application index.

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("applications/", views.ApplicationList.as_view(), name="application-index")
+    path(
+        "applications/<int:pk>/",
+        views.ApplicationDetail.as_view(),
+        name="application-detail",
+    ),
+    path("applications/", views.ApplicationList.as_view(), name="application-index"),
 ]
