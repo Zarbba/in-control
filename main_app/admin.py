b49a6361d0ae927d6b27a13995efeb7db89331a2
Zarbba <zarbba@hotmail.com>
2025-01-15 12:07:48 +1100

    Adds display for ProgressItem.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Application
+from .models import Application, ProgressItem
 
 admin.site.register(Application)
+admin.site.register(ProgressItem)
